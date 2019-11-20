7687482bf343c2200b4ec97254b9280d75dbd582
ariana124 <ariana.bibiano@gmail.com>
2019-11-20 01:57:17 +0000

    fixed braces spacing

--- path.c
+++ path.c
@@ -19,29 +19,29 @@ char *_path(char *first, char **env)
 
 		right = strtok(NULL, "=");
 
-	if (_strcmp(left, "PATH") == 0)
-	{
-		temp = strtok(right, ":");
-		while (temp) /* loops thru right value*/
-		{
-			tok_len = temp;
-		        for (j = 0; tok_len[j] != '\0'; j++)  /* loops thru token*/
-				;
-			for (k = 0; first[k] != '\0'; k++)
-				;
-			new = malloc((j + k + 2) * sizeof(char)); /* 2: slash & null */
-			if (new == NULL)
-				return (NULL);
-			_strcat(new, first);
-			_strcat(new, "/");
-			_strcat(new, first);
-			_strcat(new, "\0");
-			if (access(new, X_OK) == 0)
-				return (new);
-			temp = strtok(NULL, ":");
-			free(new);
+		if (_strcmp(left, "PATH") == 0)
+	        {
+			temp = strtok(right, ":");
+			while (temp) /* loops thru right value*/
+			{
+				tok_len = temp;
+				for (j = 0; tok_len[j] != '\0'; j++)  /* loops thru token*/
+					;
+				for (k = 0; first[k] != '\0'; k++)
+					;
+				new = malloc((j + k + 2) * sizeof(char)); /* 2: slash & null */
+				if (new == NULL)
+					return (NULL);
+				_strcat(new, first);
+				_strcat(new, "/");
+				_strcat(new, first);
+				_strcat(new, "\0");
+				if (access(new, X_OK) == 0)
+					return (new);
+				temp = strtok(NULL, ":");
+				free(new);
+			}
 		}
-	}
 	}
 	return (NULL);
 }
