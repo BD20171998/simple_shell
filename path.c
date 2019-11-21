85a1d50806e30447947b42d0cf712b812531fa94
Bob Deprizio <robert.deprizio@gmail.com>
2019-11-21 21:27:14 +0000

    Corrected path function

--- path.c
+++ path.c
@@ -3,17 +3,15 @@
 /**
  * _path - function that prints environment PATH
  * @env: environment
- * Return: path
+ * Return: 0 if successful
  */
-void _path(char *first, char **input, char **env)
+int _path(char *first, char **input, char **env)
 {
 	int token_len, first_len;
 	char *temp, *left, *right, *token;
-	char *environc = NULL, *new = NULL;
+	char *new = NULL;
 
-	environc = cpy_env(env);
-
-	left = strtok(environc, "=");
+	left = strtok(env, "=");
 	temp = strtok(NULL, "=");
 
 	if (_strcmp(left, "PATH") == 0)
@@ -25,25 +23,25 @@ void _path(char *first, char **input, char **env)
 			token_len = _strlen(token);
 			first_len = _strlen(first);
 
-			new = malloc((token_len + first_len + 2) * sizeof(char));
+			new = malloc((token_len + first_len + 2) *sizeof(char));
 			if (new == NULL)
 				return;
 			_strcat(new, right);
 			_strcat(new, "/");
 			_strcat(new, first);
 			_strcat(new, "\0");
-			if (access(new, X_OK) == 0) /*Free the copy of env*/
+			if (access(new, X_OK) == 0)
 			{
 				if (fork() == 0)
 					execve(new, input, NULL);
 				else
 					wait(NULL);
-				free(environc);
 				free(new);
+				return (0);
 			}
 			right = strtok(NULL, ":");
 			free(new);
 		}
 	}
-	exit(1);
+
 }
