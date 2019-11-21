a33bfacffc0a9676609d7761191dd556c712ba46
Bob Deprizio <robert.deprizio@gmail.com>
2019-11-21 21:17:46 +0000

    Deleted free grid helper function

--- shell.c
+++ shell.c
@@ -2,24 +2,6 @@
 #include <sys/wait.h>
 
 /**
- * free_grid - function that frees a 2 dimensional grid of int pointers
- * @grid: Char double pointer to be freed
- * @height: int for height of 2D array to be passed
- * Return: void
- */
-
-void free_grid(char **grid, int height)
-{
-
-	int k;
-
-	for (k = 0; k < height; k++)
-		free(grid[k]);
-
-	free(grid);
-}
-
-/**
  * main - Program that is simple UNIX command interpreter
  * Return: 0
  */
