ff78799d20a63bab2a8ff8f5fa2ed80c8604bb47
ecaoili24 <erika.caoili@gmail.com>
2019-11-21 00:31:00 +0000

    prints the environment

---
+++ env_copy.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * cpy_env - creates the copy of the environment variable
+ * @env: the environment
+ *
+ * Return: a character string or 0 for success
+ */
+char *cpy_env(char **env)
+{
+	char *env_cpy;
+	int i;
+
+	for (i = 0; env[i] != '\0'; i++)
+		;
+
+	env_cpy = malloc((i + 1) * sizeof(char));
+	if (env_cpy == NULL)
+		return (NULL);
+
+	_strcpy(env_cpy, env[i]);
+
+	return (0);
+}
