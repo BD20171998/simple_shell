5cd547d2b45130691dd9237f4d3258f1539f7704
ecaoili24 <erika.caoili@gmail.com>
2019-11-23 00:45:58 +0000

    added comments to putchar function

--- helpers2.c
+++ helpers2.c
@@ -35,7 +35,11 @@ int _strlen(char *s)
         return (l);
 }
 
-
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1. On error, -1 is returned..
+ */
 int _putchar(char c)
 {
         return (write(1, &c, 1));
