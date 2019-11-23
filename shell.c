3a69865f0a3ddb6f9956cba47bd294fabf9b8643
ariana124 <ariana.bibiano@gmail.com>
2019-11-23 21:40:39 +0000

    removed the built-ins lines to create seperate function

--- shell.c
+++ shell.c
@@ -35,15 +35,7 @@ int main(int argc, char **argv, char **env)
 			for (i = 0; args[i]; i++)
 				arg_num++;
 
-			if (_strcmp(args[0], "exit") == 0)
-			{
-				free(args);
-				free(line);
-				exit(EXIT_SUCCESS);
-			}
-
-			if (_strcmp(args[0], "env") == 0)
-				printenv(env);
+			builtins(line, args, env);
 
 			status = _path(args[0], args, env);
 
