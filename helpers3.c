8e8a040a2661d9bb2e3c82906cd92012df1add96
ecaoili24 <erika.caoili@gmail.com>
2019-11-24 00:57:30 +0000

    created a path str helper function

--- helpers3.c
+++ helpers3.c
@@ -40,3 +40,33 @@ int  special_char(char *buffer, ssize_t bytes)
 
 	return (0);
 }
+
+/**
+ * pathstr - function that prints the path string
+ * @right: string after "PATH ="
+ * @first: first tokenized word
+ * Return: 0 for success
+ */
+char *pathstr(char *right, char *first)
+{
+	char *new = NULL;
+	char *token = NULL;
+	int token_len = 0, first_len = 0;
+
+	token = right;
+	token_len = _strlen(token);
+	first_len = _strlen(first);
+
+	new = malloc((token_len + first_len + 2) * sizeof(char));
+	if (new == NULL)
+		return (NULL);
+
+	new[0] = '\0';
+
+	_strcat(new, right);
+	_strcat(new, "/");
+	_strcat(new, first);
+	_strcat(new, "\0");
+
+	return (new);
+}
