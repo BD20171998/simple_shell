bb99a9999b7ebab205508d399fff8999b433521c
Bob Deprizio <robert.deprizio@gmail.com>
2019-11-24 21:40:48 +0000

    Added command to flush buffer, updated while loop condition and added tally variable

--- shell.c
+++ shell.c
@@ -12,17 +12,19 @@ int main(int argc, char **argv, char **env)
 	char *prompt = "##--->";
 	char *line = NULL;
 	char **args = NULL;
-	int i = 0, status = 0, arg_num = 0;
+	int i = 0, status = 0, arg_num = 0, tally = 0;
 	size_t len = 0;
 	ssize_t read = 0;
 	(void)argc, (void)**argv;
 
-	while (read != EOF)
+	while (TRUE)
 	{
 		if (isatty(STDIN_FILENO) == 1)
 			write(STDOUT_FILENO, prompt, 6);
 
 			read = getline(&line, &len, stdin);
+
+			++tally;
 
 			if (special_char(line, read) == 127)
 				continue;
@@ -39,6 +41,9 @@ int main(int argc, char **argv, char **env)
 			status = _path(args[0], args, env);
 
 			_execute(status, line, args);
+
+			fflush(stdin);
+			printf("%i",tally);
 	}
 	free(line);
 	return (0);
