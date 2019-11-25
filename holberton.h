746ce9d549ad0e94ff6a519f028a184a9f28a88e
ariana124 <ariana.bibiano@gmail.com>
2019-11-25 23:28:59 +0000

    organized prototypes into sections

--- holberton.h
+++ holberton.h
@@ -11,40 +11,39 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-char *_strdup(char *str);
-
-char **parser(char *l);
-
-void no_nl(char *l);
-
-int _strlen(char *s);
-
-int _putchar(char c);
-
-void print_str(char *s);
-
-char *cpy_env(char **env);
-
-char *_strcpy(char *dest, char *src);
-
+/* path */
 int _path(char *first, char **input, char **env, int *ex_st);
 
-char *_strcat(char *dest, char *src);
 
-int _strcmp(char *s1, char *s2);
-
-void free_grid(char **grid, int height);
-
+/* environment */
 int printenv(char **env, int *ex_st);
+char *cpy_env(char **env);
 
-int special_char(char *buffer, ssize_t bytes, int *ex_st);
 
+/* built-ins & execute function */
+void builtins(char *line, char **args, char **env, int *ex_st);
 void _execute(int status, char **args, int *ex_st, int *tal);
 
-void builtins(char *line, char **args, char **env, int *ex_st);
 
+/* strings.c: functions related to strings */
+int _strlen(char *s);
+char *_strdup(char *str);
+int _strcmp(char *s1, char *s2);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+
+
+/* printf.c: functions related to printing strings/characters/numbers */
+int _putchar(char c);
+void print_str(char *s);
 char *pathstr(char *right, char *first);
-
+char **parser(char *l);
 void print_int(int *tal);
+
+
+/* helpers.c: helper functions */
+void no_nl(char *l);
+void free_grid(char **grid, int height);
+int special_char(char *buffer, ssize_t bytes, int *ex_st);
 
 #endif
