7678d9ecfce73e2a4b28915e742a072ee3240165
Bob Deprizio <robert.deprizio@gmail.com>
2019-11-25 03:30:01 +0000

    Updated header file for updated function prototypes

--- holberton.h
+++ holberton.h
@@ -27,7 +27,7 @@ char *cpy_env(char **env);
 
 char *_strcpy(char *dest, char *src);
 
-int _path(char *first, char **input, char **env);
+int _path(char *first, char **input, char **env, int *ex_st);
 
 char *_strcat(char *dest, char *src);
 
@@ -35,13 +35,13 @@ int _strcmp(char *s1, char *s2);
 
 void free_grid(char **grid, int height);
 
-int printenv(char **env);
+int printenv(char **env, int *ex_st);
 
-int special_char(char *buffer, ssize_t bytes);
+int special_char(char *buffer, ssize_t bytes, int *ex_st);
 
-void _execute(int status, char *line, char **args);
+void _execute(int status, char **args, int *ex_st);
 
-void builtins(char *line, char **args, char **env);
+void builtins(char *line, char **args, char **env, int *ex_st);
 
 char *pathstr(char *right, char *first);
 
