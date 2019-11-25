e3b4f9d70ca0893ed74725a3401f72187d4ce55c
Bob Deprizio <robert.deprizio@gmail.com>
2019-11-25 03:34:30 +0000

    Updated functions for exit status handling

--- built_ins.c
+++ built_ins.c
@@ -7,16 +7,16 @@
  * @env: the environment
  * Return: void
  */
-void builtins(char *line, char **args, char **env)
+void builtins(char *line, char **args, char **env, int *ex_st)
 {
 
 	if (_strcmp(args[0], "exit") == 0)
 	{
 		free(args);
 		free(line);
-		exit(EXIT_SUCCESS);
+		exit(*ex_st);
 	}
 
 	if (_strcmp(args[0], "env") == 0)
-		printenv(env);
+		printenv(env, ex_st);
 }
