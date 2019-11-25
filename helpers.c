b753ccbd8d2ac250ee06a829e1548cc35310a81c
ariana124 <ariana.bibiano@gmail.com>
2019-11-25 23:30:58 +0000

    added the special_char function

--- helpers.c
+++ helpers.c
@@ -39,3 +39,47 @@ void no_nl(char *l)
 		i++;
 	}
 }
+
+
+/**
+ * special_char - if the user types control d, it exits the shell and handles
+ * the error when the user keeps on tabbing, it carries out the command
+ * @bytes: the number of bytes read in from the user input
+ * @buffer: the buffer
+ * @ex_st: the exit status
+ * Return: Always (0) for succcess
+ */
+int  special_char(char *buffer, ssize_t bytes, int *ex_st)
+{
+	int i = 0;
+
+	if (bytes == EOF && isatty(STDIN_FILENO) == 1)
+	{
+		_putchar('\n');
+		free(buffer);
+		exit(*ex_st);
+	}
+
+	if (bytes == EOF && isatty(STDIN_FILENO) == 0)
+	{
+		free(buffer);
+		exit(*ex_st);
+	}
+
+	if (_strcmp(buffer, "\n") == 0)
+	{
+		*ex_st = 127;
+		return (127);
+	}
+
+	while (buffer[i] != '\n')
+	{
+		if (buffer[i] != ' ' && buffer[i] != '\t')
+			return (0);
+
+		++i;
+	}
+
+	*ex_st = 127;
+	return (127);
+}
