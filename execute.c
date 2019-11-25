4134d75c36aa9d2b9f1b86666bc1769f242bd8e0
Bob Deprizio <robert.deprizio@gmail.com>
2019-11-25 23:00:38 +0000

    Updated function to receive pointer for tally

--- execute.c
+++ execute.c
@@ -5,9 +5,10 @@
  * @status: the status of the path, whether the file found is executable or not
  * @args: the parsed arguments
  * @ex_st: the exit status
+ * @tal: tally of commands or attempted commands that were run
  * Return: void
  */
-void _execute(int status, char **args, int *ex_st)
+void _execute(int status, char **args, int *ex_st, int *tal)
 {
 	if (status == 2)
 	{
@@ -22,12 +23,18 @@ void _execute(int status, char **args, int *ex_st)
 		}
 		else if (access(args[0], F_OK) != 0)
 		{
+			print_str("sh: ");
+			print_int(tal);
+			print_str(": ");
 			perror(args[0]);
 			*ex_st = 127;
 		}
 		else if (access(args[0], F_OK) == 0 &&
 			 access(args[0], X_OK) != 0)
 		{
+			print_str("sh: ");
+			print_int(tal);
+			print_str(": ");
 			perror(args[0]);
 			*ex_st = 126;
 		}
